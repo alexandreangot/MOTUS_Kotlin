9ba49c832f42b6beef33817dfb17518020c7307e
Alexandre Angot <alexandre.angot@etu.unicaen.fr>
2023-01-20 21:17:02 +0100

    Adding createGrid function
    Added createGrid function for creating the try word grid

--- MOTUS/app/src/main/java/com/example/motus/MainActivity.kt
+++ MOTUS/app/src/main/java/com/example/motus/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.motus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.word)
 
         val keys = createKeyboard()
+        val word = "MAISON"
+        createGrid(word)
         /*
         val try1 = findViewById<TextView>(R.id.try1)
         for (key in keys){
@@ -23,10 +27,36 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun createGrid(word : String){
+        val tableLayoutTriedWord = findViewById<TableLayout>(R.id.tableLayoutTriedWord)
+        for (i in 0 until 6) {
+            val tableRow = TableRow(this)
+            tableRow.layoutParams = TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.MATCH_PARENT
+            )
+            tableRow.gravity = Gravity.CENTER
+            for (j in word.indices) {
+                val textView = TextView(this)
+                textView.layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    1f
+                )
+                textView.text = ""
+                textView.gravity = Gravity.CENTER
+                textView.layoutParams.height = 100
+                textView.layoutParams.width = 50
+                textView.setBackgroundResource(R.drawable.textview_border)
+                tableRow.addView(textView)
+            }
+            tableLayoutTriedWord.addView(tableRow)
+        }
+    }
 
     private fun createKeyboard(): MutableList<Button> {
         val buttonList = mutableListOf<Button>()
-        val tableLayout = findViewById<TableLayout>(R.id.clavier)
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayoutKeyboard)
 
         val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
         var index = 0
